c67ad1d0492fa696969ae83e2e37f144889bb6c6
Siva Sivapalan <ssivapaul@gmail.com>
2024-10-26 01:47:30 +1100

    axis creating exercise....

--- D3.js
+++ D3.js
@@ -2,14 +2,19 @@ var dataArray = [20, 40, 50, 60];
 var w = 500;
 var h = 500;
 
+var barScale = d3.scaleLinear().domain([0, 60]).range([0, w]);
+var color = d3.scaleLinear().domain([0, 60]).range(["red", "blue"]);
+var axis = d3.axisBottom(barScale);
+
 var canvas = d3
   .select("body")
   .append("svg")
+  .attr("style", "border: 2px solid")
   .attr("width", w)
   .attr("height", h)
-  .append("g");
-var barScale = d3.scaleLinear().domain([0, 60]).range([0, w]);
-var color = d3.scaleLinear().domain([0, 60]).range(["red", "blue"]);
+  .append("g")
+  .attr("transform", "translate(20, 0)")
+  .call(axis);
 
 var bars = canvas
   .selectAll("rect")
