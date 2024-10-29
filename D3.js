6270f9dab78c44f1be5927f7af33829f29bbc8a6
Siva Sivapalan <ssivapaul@gmail.com>
2024-10-30 01:14:17 +1100

    Tutorial 10. Loading external data final commit

--- D3.js
+++ D3.js
@@ -4,19 +4,35 @@ d3.json("mydata.json").then((data) => {
     .select("body")
     .append("svg")
     .attr("width", 500)
-    .attr("height", 500);
+    .attr("height", 500)
+    .attr("style", "border:1px solid");
 
   canvas
     .selectAll("rect")
+    .attr("position", "absolute")
     .data(data)
     .enter()
     .append("rect")
     .attr("width", function (d) {
       return d.age * 5;
     })
-    .attr("height", 20)
+    .attr("height", 30)
     .attr("y", function (d, i) {
-      return i * 25;
+      return i * 50;
     })
     .attr("fill", "blue");
+
+  canvas
+    .selectAll("text")
+    .attr("position", "relative")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("fill", "white")
+    .attr("y", function (d, i) {
+      return i * 50 + 20;
+    })
+    .text(function (d) {
+      return d.name;
+    });
 });
