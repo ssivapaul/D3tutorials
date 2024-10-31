848e544b3ce74e368f593f0cef7aa08407bde3e5
Siva Sivapalan <ssivapaul@gmail.com>
2024-10-31 20:05:09 +1100

    Tutorial 13. The Pie layout, first commit

--- D3.js
+++ D3.js
@@ -1,20 +1,26 @@
 var canvas = d3
   .select("body")
   .append("svg")
-  .attr("width", 500)
-  .attr("height", 500)
+  .attr("width", 1500)
+  .attr("height", 1500)
   .attr("style", "border:1px solid");
 
-var group = canvas.append("g").attr("transform", "translate(100, 100)");
+var group = canvas.append("g").attr("transform", "translate(300, 300)");
 
-var r = 100;
-var p = Math.PI * 2;
+var r = 300;
+var data = [10, 50, 80];
 
-var arc = d3
-  .arc()
-  .innerRadius(r - 10)
-  .outerRadius(r)
-  .startAngle(0)
-  .endAngle(p - 1);
+var arc = d3.arc().innerRadius(200).outerRadius(r);
 
-group.append("path").attr("d", arc);
+var pie = d3.pie().value(function (d) {
+  return d;
+});
+
+var arcs = group
+  .selectAll(".arc")
+  .data(pie(data))
+  .enter()
+  .append("g")
+  .attr("class", "arc");
+
+arcs.append("path").attr("d", arc);
